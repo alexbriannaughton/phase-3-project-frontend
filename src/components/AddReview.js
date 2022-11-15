0d69cd56e2dd617f5692ef9f6467df08f7f7e931
Kade Hess <kadeahess@gmail.com>
2022-11-15 11:55:10 -0800

    addreview set up

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -1,12 +1,70 @@
-import React from "react";
+import React, {useState} from 'react'
 
-function AddReview() {
+function AddReview ({addReviewer, house_id}) {
+    const [author, authorChanger] = useState("")
+    const [body, bodyChanger] = useState("")
+    const [stars, starsChanger] = useState("")
 
-    return(
-        <div>
-            <h1>this is add review page</h1>
-        </div>
+    function submitHandler(e) {
+        e.preventDefault()
+
+        const newReview = {
+            author: author,
+            body: body,
+            stars: stars,
+            house_id: house_id
+        }
+
+        fetch("http://localhost:3000/add-review", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newReview)
+        })
+        .then(resp => resp.json())
+        .then(data => addReviewer(data))
+
+        authorChanger("")
+        bodyChanger("")
+        starsChanger("")
+    }
+
+    return (
+        <>
+        <h1>Add your review of this house!</h1>
+        <form onSubmit={submitHandler}>
+            <label>Name</label>
+            <input
+                type="text"
+                name="author"
+                label="author"
+                value={author}
+                onChange={(e) => authorChanger(e.target.value)}
+            />
+            <label>Review</label>
+            <textarea
+                className="big-input"
+                type="text"
+                rows="3"
+                name="body"
+                label="body"
+                value={body}
+                onChange={(e) => bodyChanger(e.target.value)}
+            ></textarea>
+            <label>Santas</label> 
+            <input
+                type="text"
+                name="santas"
+                label="santas"
+                value={stars}
+                onChange={(e) => starsChanger(e.target.value)}
+            />
+            <button type="submit">Submit Review</button>
+        </form>
+        </>
+  
     )
-}
+    }
 
 export default AddReview
